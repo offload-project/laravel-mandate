d2db961b839081a86426ec8b3daf86b8af0e358d
Shavonn Brown <brown.shevy7@gmail.com>
2026-01-20 16:25:19 -0500

    feat: capability can apply to entire permission class

--- src/Attributes/Capability.php
+++ src/Attributes/Capability.php
@@ -9,17 +9,26 @@ use Attribute;
 /**
  * Assigns a permission to a capability during sync.
  *
- * Multiple Capability attributes can be applied to assign a permission to multiple capabilities.
+ * Can be applied at the class level to assign all permissions in the class to a capability,
+ * or at the constant level for individual permissions. Multiple attributes can be applied
+ * to assign a permission to multiple capabilities.
  *
- * @example
+ * @example Class-level (all permissions inherit):
+ * #[Capability('user-management')]
+ * class UserPermissions
+ * {
+ *     public const VIEW = 'user:view';
+ *     public const EDIT = 'user:edit';
+ * }
+ * @example Constant-level:
  * #[Capability('user-management')]
  * public const VIEW = 'user:view';
- *
+ * @example Multiple capabilities:
  * #[Capability('user-management')]
  * #[Capability('reporting')]
  * public const EXPORT = 'user:export';
  */
-#[Attribute(Attribute::TARGET_CLASS_CONSTANT | Attribute::IS_REPEATABLE)]
+#[Attribute(Attribute::TARGET_CLASS | Attribute::TARGET_CLASS_CONSTANT | Attribute::IS_REPEATABLE)]
 final readonly class Capability
 {
     public function __construct(
